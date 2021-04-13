22384b2d7a2ed91ff8ff8e6464976e3a3c0e787e
fre-ben <fschocke@gmail.com>
2021-04-13 10:01:50 +0200

    🔥 Remove unnecessary components

--- utils/math.ts
+++
@@ -1,9 +0,0 @@
-export function calculatePercentage(value: number, max: number): string {
-  const result = Math.round((value / max) * 100);
-
-  if (result > 100) {
-    return "Invalid input";
-  }
-
-  return result + "%";
-}
