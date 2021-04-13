55ee5c2049a0a4ab805a9bafe8e3e6a16f50f782
fre-ben <fschocke@gmail.com>
2021-04-13 14:57:32 +0200

    ✨ Add mongodb setup

---
+++ server/db.ts
@@ -0,0 +1,19 @@
+import { Db, MongoClient } from "mongodb";
+
+let client: MongoClient = null;
+let db: Db = null;
+
+const url = process.env.MONGODB_URL;
+
+export async function connectDB(url: string, dbName: string) {
+  client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  db = client.db(dbName);
+}
+
+export async function getTasks() {
+  await connectDB(url, "timetracker");
+
+  const tasks = await db.collection("tasks").find({}).toArray();
+
+  return tasks;
+}
